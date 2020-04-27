20c14332ba8443a437bed5f432c85b9ec37db421
ishvar99 <ishanvarshney99@gmail.com>
2020-04-28 02:39:12 +0530

    fetching data from API

--- src/App.js
+++ src/App.js
@@ -4,12 +4,28 @@ import axios from 'axios';
 import {Cards,Charts,CountryPicker} from './Components/index'; 
 
 class App extends Component {
+  state={
+    data:{}
+  }
+  async componentDidMount(){
+    try {
+      const response=await(axios.get('https://covid19.mathdro.id/api'));
+      console.log(response.data);
+      const {data:{confirmed,recovered,deaths,lastUpdate}}=response;
+    const modifiedData={confirmed,recovered,deaths,lastUpdate}
+    console.log(modifiedData);
+    this.setState({data:modifiedData});
+    } catch (error) {
+      this.setState({data:{error:'Failed to fetch data'}})
+    } 
+  }
 render()
   {
+    const {data}=this.state;
   return (
     <div>
       <CountryPicker/>
-      <Cards/>
+      <Cards fetchedData={data}/>
       <Charts/>
     </div>
   );
--- src/Components/Cards/cards.jsx
+++ src/Components/Cards/cards.jsx
@@ -1,38 +1,44 @@
 import React from 'react';
 import {Grid,Card,CardContent,Typography} from '@material-ui/core'; 
+import classnames from 'classnames';
+import CountUp from 'react-countup';
 import styles from './cards.module.css'
-const Cards=()=>{
+const Cards=(props)=>{
+  const {fetchedData:{confirmed,recovered,deaths,lastUpdate}}=props;
+  if(!confirmed){
+    return 'Loading....'
+  }
   return (
-      <div className="styles.container">
-    <Grid container spacing={2}>
-      <Grid item component={Card}>
+         <div className="styles.container">
+    <Grid container spacing={2} justify="center">
+      <Grid item component={Card} xs={12} className={classnames(styles.card,styles.infected)}>
         <CardContent>
             <Typography color="textSecondary" gutterBottom>
                 Infected
                 </Typography> 
-                <Typography variant="h5">REAL DATA</Typography>
-                <Typography color="textSecondary">REAL DATE</Typography>    
+                <Typography variant="h5"><CountUp end={confirmed.value} duration={2} seperator=","/></Typography>
+                <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>    
                 <Typography variant="body2">Number of active cases of COVID-19</Typography>
         </CardContent>    
       </Grid>
-      <Grid item justify='center' component={Card}>
+      <Grid item component={Card} xs={12} className={classnames(styles.card,styles.infected)}>
       <CardContent>
             <Typography color="textSecondary" gutterBottom>
-                Infected
+                Recovered
                 </Typography> 
-                <Typography variant="h5">REAL DATA</Typography>
-                <Typography color="textSecondary">REAL DATE</Typography>    
-                <Typography variant="body2">Number of active cases of COVID-19</Typography>
+                <Typography variant="h5"><CountUp end={recovered.value} duration={2} seperator=","/></Typography>
+                <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>    
+                <Typography variant="body2">Number of recovered cases of COVID-19</Typography>
         </CardContent>    
       </Grid>
-      <Grid item component={Card}>
+      <Grid item component={Card} xs={12} className={classnames(styles.card,styles.infected)}>
       <CardContent>
             <Typography color="textSecondary" gutterBottom>
-                Infected
+                Deaths
                 </Typography> 
-                <Typography variant="h5">REAL DATA</Typography>
-                <Typography color="textSecondary">REAL DATE</Typography>    
-                <Typography variant="body2">Number of active cases of COVID-19</Typography>
+      <Typography variant="h5"><CountUp end={deaths.value} duration={2} seperator=","/></Typography>
+                <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>    
+                <Typography variant="body2">Number of death cases of COVID-19</Typography>
         </CardContent>    
       </Grid>
     </Grid>
