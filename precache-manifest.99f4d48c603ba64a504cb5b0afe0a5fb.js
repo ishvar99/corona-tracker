5c4f5615b45563ae63cf2a326ec3617d5827cbb3
ishvar99 <ishanvarshney99@gmail.com>
2020-04-30 15:26:26 +0530

    Updates

---
+++ precache-manifest.99f4d48c603ba64a504cb5b0afe0a5fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78078eed54687957879d846fbd8e749a",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "08f46d2089a6c9e0b5ce",
+    "url": "/corona-tracker/static/css/main.21691c1a.chunk.css"
+  },
+  {
+    "revision": "d5e4b50e427a3bb38106",
+    "url": "/corona-tracker/static/js/2.830183be.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-tracker/static/js/2.830183be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08f46d2089a6c9e0b5ce",
+    "url": "/corona-tracker/static/js/main.49bc4989.chunk.js"
+  },
+  {
+    "revision": "4127b310accafee69bc6",
+    "url": "/corona-tracker/static/js/runtime-main.cfc71fe3.js"
+  },
+  {
+    "revision": "d5049d728f9f831efdd5cba3a8ae71f0",
+    "url": "/corona-tracker/static/media/covid19.d5049d72.jpg"
+  }
+]);
